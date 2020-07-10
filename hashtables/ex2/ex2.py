3dd81f3691a5c2c468adf4d96b1ea6cb88a8e32e
Jojo Zhang <jojozhang0327@gmail.com>
2020-07-10 12:35:04 -0400

    complete ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,19 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
-
+    route = []
+    flights = {}
+    # if source is none, it's the head
+    # if des is none, it's the tail
+    # bulid my ticket dictionary
+    for ticket in tickets:
+        flights[ticket.source] = ticket.destination
+    # initialize my router
+    curr_flight = flights["NONE"]
+    # while not the last flight
+    # append the current fight
+    while curr_flight != "NONE":
+        route.append(curr_flight)
+        curr_flight = flights[curr_flight]
+    route.append(curr_flight)
     return route
